3c7f46fd69e0b8029ce804db6e17c8b35d31d980
Tanaji Langote <102846255+TanajiLangote@users.noreply.github.com>
2025-02-21 12:07:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import tgcrypto
+from pyrogram import Client, filters
+
+# Replace 'my_bot' with your bot's name, and provide your API ID and hash
+app = Client("7828276514:AAG5JojoEFTj2o4Js9sqXgigEe2vVMR1MmA", api_id='13485297', api_hash='4858ef14a1e4e5d1f496560f33ec9cb4')
+
+@app.on_edited_message()
+async def delete_edited_message(Client, message):
+        await message.delete()  # Deletes the edited message
+
+if name == "main":
+    app.run()
